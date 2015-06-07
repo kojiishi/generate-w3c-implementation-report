a23c9a9b11ca8056c72eee46703c9a385426dcc0
Koji Ishii <kojii@chromium.org>
2015-06-08 01:20:34 +0900

    Output format updated, 3 platform failures as fail

--- generate_css_implementation_report.py
+++ generate_css_implementation_report.py
@@ -25,7 +25,8 @@ def main():
         logging.basicConfig(level=logging.WARNING)
     args.dir = os.path.expanduser(args.dir)
     generator = W3CImplementationReportGenerator()
-    generator.load_test_results(args.dir)
+    generator.load_test_files(args.dir)
+    generator.load_test_results(os.path.join(args.dir, '../../../TestExpectations'))
     if not args.output or args.output == '-':
         generator.write_report(args.template, sys.stdout)
     else:
@@ -34,53 +35,58 @@ def main():
 
 class W3CImplementationReportGenerator(object):
     def __init__(self):
-        pass
+        self.tests = {}
 
     class Test(object):
         def __init__(self, name):
             self.combo = None
-            self.comment = None
+            self._comment = None
             self.import_dir = None
             self.import_ext = None
-            self._is_failed = False
+            self._is_fail = False
+            self._fail_conditions = set()
             self.name = name;
             self.testnames = []
 
         @property
-        def is_failed(self):
-            return self._is_failed or self.combo and self.combo._is_failed
-
-        @is_failed.setter
-        def is_failed(self, value):
-            self._is_failed = value
-
-        @property
-        def is_imported(self):
+        def is_import(self):
             return self.import_dir or self.combo and self.combo.import_dir
 
-    def load_test_results(self, directory):
-        tests = {}
+        @property
+        def is_fail(self):
+            return self._is_fail or len(self._fail_conditions) >= 3 or self.combo and self.combo.is_fail
+
+        @property
+        def comment(self):
+            if len(self._fail_conditions) == 0 or self.is_fail:
+                return self._comment
+            return ', '.join(self._fail_conditions) + ': ' + (self._comment if self._comment else 'Fail')
+
+        def set_result(self, conditions, is_pass, comment):
+            if not is_pass:
+                if not conditions:
+                    self._is_fail = True
+                else:
+                    for condition in conditions:
+                        self._fail_conditions.add(condition)
+            else:
+                if conditions:
+                    comment = ', '.join(conditions) + ': ' + comment
+            self._comment = comment
+
+    def add_test(self, name):
+        if name in self.tests:
+            raise Error("File name conflicts: " + name)
+        test = W3CImplementationReportGenerator.Test(name)
+        self.tests[name] = test
+        return test
+
+    def load_test_files(self, directory):
         for root, filename in self.find_test_files(directory):
-            if filename in tests:
-                log.warn("File name conflicts: %s", filename)
             name, ext = os.path.splitext(filename)
-            test = W3CImplementationReportGenerator.Test(name)
+            test = self.add_test(name)
             test.import_dir = root
             test.import_ext = ext
-            tests[name] = test
-
-        expectations = os.path.join(directory, '../../../TestExpectations')
-        for path, is_pass, comment in self.read_test_expectations(expectations):
-            filename = os.path.basename(path)
-            name, ext = os.path.splitext(filename)
-            test = tests.get(name, None)
-            if not test:
-                log.warn("Test for a failure not found: %s", filename)
-                continue
-            test.is_failed = not is_pass
-            test.comment = comment
-
-        self.tests = tests
 
     def find_test_files(self, directory):
         dirs_to_skip = ('support',)
@@ -95,6 +101,16 @@ class W3CImplementationReportGenerator(object):
                     continue
                 log.debug("Test file found: %s %s", root, file)
                 yield (root, file)
+
+    def load_test_results(self, expectations):
+        for path, conditions, is_pass, comment in self.read_test_expectations(expectations):
+            filename = os.path.basename(path)
+            name, ext = os.path.splitext(filename)
+            test = self.tests.get(name, None)
+            if not test:
+                log.warn("Test for a failure not found: %s", filename)
+                continue
+            test.set_result(conditions, is_pass, comment)
 
     def read_test_expectations(self, path):
         pattern = re.compile(r'([^\[]+)(\[[^\]]+])?\s+(\S+)\s+\[\s*([^\]]+)]$')
@@ -117,20 +133,17 @@ class W3CImplementationReportGenerator(object):
                     if not path.startswith('imported/csswg-test/css-writing-modes-3/'):
                         continue
                     results = results.rstrip().split()
+                    conditions = conditions.strip('[]').split() if conditions else None
                     if 'Pass' in results:
                         log.info("Flaky as Pass: %s", line)
-                        yield (path, True, 'Flaky')
-                        continue
-                    if conditions:
-                        log.info("Conditional as Pass: %s", line)
-                        yield (path, True, conditions + (' ' + comment if comment else ''))
+                        yield (path, conditions, True, 'Flaky')
                         continue
                     if is_actually_pass:
                         log.info("Fail as pass: %s # %s", line, comment)
-                        yield (path, True, comment)
+                        yield (path, conditions, True, comment)
                         continue
                     log.debug("Fail Expectation found: %s %s %s", conditions, path, results)
-                    yield (path, False, None)
+                    yield (path, conditions, False, None)
                     continue
                 log.warn("Line unknown: %s", line)
 
@@ -155,7 +168,7 @@ class W3CImplementationReportGenerator(object):
             test = self.get_test_from_testname(values[0])
             if not test:
                 return None
-            if test.is_failed:
+            if test.is_fail:
                 values[1] = 'fail'
             else:
                 values[1] = 'pass'
@@ -171,8 +184,7 @@ class W3CImplementationReportGenerator(object):
         name, ext = os.path.splitext(filename)
         test = self.tests.get(name, None)
         if not test:
-            test = W3CImplementationReportGenerator.Test(name)
-            self.tests[name] = test
+            test = self.add_test(name)
         test.testnames.append(testname)
         if not test.combo and re.search(r'-\d{3}[a-z]$', name): # if affix, find the combo test
             test.combo = self.tests.get(name[0:-1], None)
@@ -189,9 +201,9 @@ class W3CImplementationReportGenerator(object):
                 log.warn('Not found in template: %s', filename)
                 continue
             total += 1
-            if test.is_imported:
+            if test.is_import:
                 coverage += 1
-                if test.is_failed:
+                if test.is_fail:
                     fail += 1
         log.info("Total = %d, Coverage = %d, Fail= %d", total, coverage, fail)
         return (total, coverage, fail)
