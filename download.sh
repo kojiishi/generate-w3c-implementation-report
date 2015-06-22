839082b12874d5eeb7429d8f7676f267daf66d8c
Koji Ishii <kojii@chromium.org>
2015-06-22 19:19:17 +0900

    Sort results.csv to ensure diff is sensible

--- download.sh
+++ download.sh
@@ -4,4 +4,6 @@ curl -O http://test.csswg.org/suites/css-writing-modes-3_dev/nightly-unstable/im
 curl -O http://test.csswg.org/suites/css-writing-modes-3_dev/nightly-unstable/results.zip
 unzip -o results.zip
 rm results.zip
+(head -n 1 results.csv && tail -n +2 results.csv | sort -t , -k 4) > results_sorted.csv
+mv -f results_sorted.csv results.csv
 cd ..
