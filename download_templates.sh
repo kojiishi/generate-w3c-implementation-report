a074f36dfbc426ebc42c6a24d6e924b794c9e1f6
Koji Ishii <kojiishi@gmail.com>
2015-12-03 21:46:44 +0900

    Add testinfo.data

--- download_templates.sh
+++ download_templates.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd css-writing-modes-3
 curl -O http://test.csswg.org/suites/css-writing-modes-3_dev/nightly-unstable/implementation-report-TEMPLATE.data
-git diff implementation-report-TEMPLATE.data
+curl -O http://test.csswg.org/suites/css-writing-modes-3_dev/nightly-unstable/testinfo.data
+git diff *.data
 cd ..
